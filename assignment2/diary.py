8e1e988d9d8e0db8115799c55885be2636e8ca54
Kurstin King <Kurstin_King@outlook.com>
2026-07-17 12:40:04 -0400

    fixed exception handling structure

--- assignment2/diary.py
+++ assignment2/diary.py
@@ -1,4 +1,5 @@
 import traceback
+import sys
 
 
 
@@ -15,9 +16,18 @@ try:
            prompt = "What else? "
 
 except Exception as e:
-  print('An exception occurred.')
-  print(type(e).__name__)
+ 
   trace_back = traceback.extract_tb(e.__traceback__)
+  stack_trace = list()
+
+  for trace in trace_back:
+    stack_trace.append(f'File : {trace[0]}, Line : {trace[1]}, Func.Name : {trace[2]}, Message : {trace[3]}')
+  print(f'Exception type: {type(e).__name__}')
+  message = str(e)
+  if message:
+    print(f'Exception message: {message}')
+  print(f'Stack trace: {stack_trace}')
+  sys.exit(1)
         
 
 
